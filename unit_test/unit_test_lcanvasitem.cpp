fa1196f6c16a1c7b5702ad0e665215d7681cbcf8
lws <lwscode@163.com>
2021-11-22 01:36:30 +0800

    2021/11/22

--- unit_test/unit_test_lcanvasitem.cpp
+++
@@ -1,6 +0,0 @@
-#include "unit_test_lcanvasitem.h"
-
-unit_test_lcanvasitem::unit_test_lcanvasitem()
-{
-
-}
--- unit_test/unit_test_lcanvasview.cpp
+++
@@ -1,6 +0,0 @@
-#include "unit_test_lcanvasview.h"
-
-unit_test_lcanvasview::unit_test_lcanvasview()
-{
-
-}
